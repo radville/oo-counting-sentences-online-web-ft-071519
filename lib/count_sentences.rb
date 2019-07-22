75ababcc9ba08609591aed474885873a91cb6d72
Laura Radville <laura.radville@gmail.com>
2019-07-22 19:02:01 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,29 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    count = 0
+    self.split(/[\.?!]/).each do |el|
+      if el.length > 1
+        puts el
+        count += 1
+      end
+    end
+    count 
   end
+  
 end
+
+complex_string = "This, well, is a sentence. This is too!! And so is this, I think? Woo..."
+puts complex_string.count_sentences
